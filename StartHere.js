27b76c82907c7835f24061f715dda72f7bc18068
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-20 05:38:54 -0600

    Update StartHere.js

--- StartHere.js
+++ StartHere.js
@@ -6,9 +6,7 @@ export async function main(ns) {
 	// My Stuff
 	var githubURL = "https://raw.githubusercontent.com/TheDroidYourLookingFor/BitBurner-Scripts/main/initDownload.js";
 	var outputFileName = "/TheDroid/initDownload.js";
-	var launchAfterDL = false;
-	var launchThreads = 1;
-	
-	await ns.wget(githubURL,outputFileName);
-	if (launchAfterDL) ns.spawn("initDownload.js", launchThreads);
+
+	await ns.wget(githubURL, outputFileName);
+	ns.spawn(outputFileName, 1);
 }
