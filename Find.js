066219be4a4408c537cb07d706b64d5ea1797ae2
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-20 06:38:01 -0600

    Update Find.js

--- Find.js
+++ Find.js
@@ -7,8 +7,6 @@ export async function main(ns) {
 		var myHackLevel = ns.getHackingLevel();
 		var bestTargetIndex = 15;
 		var bestTargetScore = 0;
-		var lastTarget;
-		var winnerwinnerchickendinner;
 
 		var numBusters = 0;
 		var portBusters = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe'];
@@ -19,26 +17,17 @@ export async function main(ns) {
 		var rows = await ns.read("/TheDroid/nmap.txt").split("\r\n");
 		for (var i = 0; i < rows.length; ++i) {
 			var serverData = rows[i].split(',');
-			if (serverData.length < 7) break;
+			if (serverData.length < 9) break;
 			var svName = serverData[0];
-			ns.tprint(svName);
 			var svRam = serverData[1];
-			ns.tprint(svRam);
 			var svPortsNeeded = serverData[2];
-			ns.tprint(svPortsNeeded);
 			var svMinSec = serverData[3];
-			ns.tprint(svMinSec);
 			var svReqHack = serverData[4];
-			ns.tprint(svReqHack);
 			var svExecTime = serverData[5];
-			ns.tprint(svExecTime);
 			var svCurMoney = serverData[6];
-			ns.tprint(svCurMoney);
 			var svMaxMoney = serverData[7];
-			ns.tprint(svMaxMoney);
 			var svGrowth = serverData[8];
-			ns.tprint(svGrowth);
-			lastTarget = svName;
+			var svScore;
 
 			if (!(ns.hasRootAccess(svName)) && (numBusters >= svPortsNeeded) && (myHackLevel >= svReqHack)) {
 				if (numBusters > 0) ns.brutessh(svName);
@@ -52,20 +41,27 @@ export async function main(ns) {
 
 			if (ns.hasRootAccess(svName)) {
 				if (svCurMoney < 50000) {
-					// write better code in the future here
+					await ns.write("/TheDroid/Broke-Targets.txt", svName
+						+ "," + ns.getServerMaxRam(svName)
+						+ "," + ns.getServerNumPortsRequired(svName)
+						+ "," + ns.getServerMinSecurityLevel(svName)
+						+ "," + ns.getServerRequiredHackingLevel(svName)
+						+ "," + ns.getHackTime(svName)
+						+ "," + ns.getServerMoneyAvailable(svName)
+						+ "," + ns.getServerMaxMoney(svName)
+						+ "," + ns.getServerGrowth(svName)
+						+ "\r\n");
 				} else {
 					svScore = ((svMaxMoney * 100 / svGrowth) / svExecTime);
 					if (svScore > bestTargetScore) {
 						ns.print("New High Score: " + bestTargetScore);
 						bestTargetScore = svScore;
 						bestTargetIndex = i;
-						winnerwinnerchickendinner = svName;
 					}
 				}
 			}
 			ns.print(i);
 		}
-		
 		await ns.write("/TheDroid/best_target.txt", rows[bestTargetIndex], "w");
 		outputStats(ns, svName);
 	}
