dd39f6a278bf1dd55d3a836700474f655c124cf3
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-22 03:36:50 -0600

    Update Distribute.js

--- Distribute.js
+++ Distribute.js
@@ -1,107 +1,103 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	const usrDirectory = "/TheDroid/";
+	const args = ns.flags([['help', false]]);
+	if (args.help) {
+		ns.tprint("This script will distribute our scripts to any hosts not running them.");
+		ns.tprint(`USAGE: run ${ns.getScriptName()}`);
+		ns.tprint("Example:");
+		ns.tprint(`> run ${ns.getScriptName()}`);
+		return;
+	}
 	const useDebug = false;
-	const usrProbeData = usrDirectory + "best_target.txt";
-	const usrProbeData2 = usrDirectory + "networkProbeData.txt";
-
-	if (useDebug) ns.tail(usrDirectory + "Find.js", "home");
+	if (useDebug) ns.tail(ns.getScriptName())
+	const usrDirectory = "/TheDroid/";
+	const usrProbeData = new String("networkProbeData.txt");
+	const usrProbeData2 = new String("broke_Targets.txt");
+	var hackScripts = [usrDirectory + "weaken.js", usrDirectory + "hack.js", usrDirectory + "grow.js", usrDirectory + "aio.js"];
+	var hack_mem = ns.getScriptRam(usrDirectory + "weaken.js", "home");
+	var aio_mem = ns.getScriptRam(usrDirectory + "aio.js", "home");
 
 	/** @param {NS} ns **/
-	async function lookForTargets(ns) {
-		var myHackLevel = ns.getHackingLevel();
-		var bestTargetIndex = 15;
-		var bestTargetScore = 0;
+	async function uploadToHost(svHost) {
+		if (useDebug) ns.tprint("Killing all processes on " + svHost);
+		ns.killall(svHost);
+		await ns.asleep(25);
+		if (useDebug) ns.tprint("Copying hackscripts to " + svHost);
+		await ns.scp(hackScripts, "home", svHost);
+		await ns.asleep(25);
+	}
+	/** @param {NS} ns **/
+	async function beginNetworkAttack(ns, fileName) {
+		if (useDebug) ns.tprint("Hack Memory: " + hack_mem);
+		if (useDebug) ns.tprint("AIO Memory: " + aio_mem);
 
-		var numBusters = 0;
-		var portBusters = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe'];
-		for (var i = 0; i < portBusters.length; i++) {
-			if (ns.fileExists(portBusters[i], "home")) ++numBusters;
-		}
+		var weakenThreadWeight = 15;
+		var hackThreadWeight = 55;
+		var growThreadWeight = 30;
 
-		var rows = await ns.read(usrProbeData2).split("\r\n");
+		var bestTarget = await ns.read(usrDirectory + "best_target.txt").split(",");
+		var tName = bestTarget[0];
+		if (useDebug) ns.tprint("Best Target: " + tName);
+
+		var rows = await ns.read(usrDirectory + fileName).split("\r\n");
 		for (var i = 0; i < rows.length; ++i) {
 			var serverData = rows[i].split(',');
 			if (serverData.length < 9) break;
 			var svName = serverData[0];
-			var svRam = serverData[1];
-			var svPortsNeeded = serverData[2];
-			var svMinSec = serverData[3];
-			var svReqHack = serverData[4];
-			var svExecTime = serverData[5];
-			var svCurMoney = serverData[6];
-			var svMaxMoney = serverData[7];
-			var svGrowth = serverData[8];
-			var svScore;
+			var svRamAvail = ns.getServerMaxRam(svName);
+			var num_threads = Math.floor(svRamAvail / hack_mem);
 
-			if (!(ns.hasRootAccess(svName)) && (numBusters >= svPortsNeeded) && (myHackLevel >= svReqHack)) {
-				if (numBusters > 0) ns.brutessh(svName);
-				if (numBusters > 1) ns.ftpcrack(svName);
-				if (numBusters > 2) ns.relaysmtp(svName);
-				if (numBusters > 3) ns.httpworm(svName);
-				if (numBusters > 4) ns.sqlinject(svName);
-				ns.nuke(svName);
-				if (useDebug) ns.tprint("Server hacked: " + svName);
-			}
+			if (ns.hasRootAccess(svName) && svName != "home") {
+				if (num_threads >= 6) {
+					if (useDebug) ns.tprint("Beginning multithreaded attack on" + tName + " from " + svName);
+					if ((num_threads & 1) != 0) num_threads = num_threads - hack_mem;
+					var hack_threads = Math.round(((hackThreadWeight / 100) * num_threads));
+					var grow_threads = Math.floor(((growThreadWeight / 100) * num_threads));
+					var weaken_threads = Math.floor(((weakenThreadWeight / 100) * num_threads));
 
-			if (ns.hasRootAccess(svName)) {
-				if (svCurMoney < 50000) {
-					// await ns.write(usrDirectory + "broke_Targets.txt", svName
-					// 	+ "," + ns.getServerMaxRam(svName)
-					// 	+ "," + ns.getServerNumPortsRequired(svName)
-					// 	+ "," + ns.getServerMinSecurityLevel(svName)
-					// 	+ "," + ns.getServerRequiredHackingLevel(svName)
-					// 	+ "," + ns.getHackTime(svName)
-					// 	+ "," + ns.getServerMoneyAvailable(svName)
-					// 	+ "," + ns.getServerMaxMoney(svName)
-					// 	+ "," + ns.getServerGrowth(svName)
-					// 	+ "\r\n");
+					await uploadToHost(svName);
+					if (useDebug) ns.tprint("Executing " + hackScripts[1] + " with " + hack_threads + " threads on " + tName + " from " + svName);
+					ns.exec(hackScripts[1], svName, hack_threads, tName);
+					if (useDebug) ns.tprint("Executing " + hackScripts[0] + " with " + weaken_threads + " threads on " + tName + " from " + svName);
+					ns.exec(hackScripts[0], svName, weaken_threads, tName);
+					if (useDebug) ns.tprint("Executing " + hackScripts[2] + " with " + grow_threads + " threads on " + tName + " from " + svName);
+					ns.exec(hackScripts[2], svName, grow_threads, tName);
+					await ns.asleep(100);
+
+					svRamAvail = ns.getServerMaxRam(svName);
+					var svRamUsed = ns.getServerUsedRam(svName);
+					var num_threads = (svRamAvail - svRamUsed) / hack_mem;
+					if (num_threads > 0) {
+						ns.kill(hackScripts[1], svName, tName);
+						if (useDebug) ns.tprint("Executing " + hackScripts[1] + " with " + num_threads + " threads on " + tName + " from " + svName);
+						ns.exec(hackScripts[1], svName, (hack_threads + num_threads), tName);
+					}
 				} else {
-					svScore = ((svMaxMoney * 100 / svGrowth) / svExecTime);
-					if (svScore > bestTargetScore) {
-						if (useDebug) ns.print("New High Score: " + bestTargetScore);
-						bestTargetScore = svScore;
-						bestTargetIndex = i;
+					if (svRamAvail > aio_mem) {
+						num_threads = Math.floor(svRamAvail / aio_mem);
+						if (useDebug) ns.tprint("Beginning low memory attack script on " + tName + " from " + svName);
+						await uploadToHost(svName);
+						if (num_threads > 0) {
+							if (useDebug) ns.tprint("Executing " + hackScripts[3] + " with " + num_threads + " threads on " + tName + " from " + svName);
+							ns.exec(hackScripts[3], svName, num_threads, tName);
+						}
 					}
 				}
+
 			}
-			ns.print(i);
+			ns.asleep(25);
 		}
-		await ns.write(usrProbeData, rows[bestTargetIndex], "w");
-		if (useDebug) outputStats(ns, svName);
+	}
+	/** @param {NS} ns **/
+	async function processNmap(ns) {
+
+		if (useDebug) ns.tprint("Beginning distribution of scripts to all servers.");
+		if (useDebug) ns.tprint("Reading " + usrProbeData2);
+		await beginNetworkAttack(ns, usrProbeData2);
+		if (useDebug) ns.tprint("Reading " + usrProbeData);
+		await beginNetworkAttack(ns, usrProbeData);
+		if (useDebug) ns.tprint("Finished distributing scripts to all servers.");
 	}
 
-	async function outputStats(ns, svrName) {
-		var svName = svrName;
-		var svRAM = ns.getServerMaxRam(svName);
-		var svPorts = ns.getServerNumPortsRequired(svName);
-		var svReqHack = ns.getServerRequiredHackingLevel(svName);
-		var svMinSec = ns.getServerMinSecurityLevel(svName);
-		var svGrowth = ns.getServerGrowth(svName);
-		var svCurMoney = ns.getServerMoneyAvailable(svName);
-		var svMaxMoney = ns.getServerMaxMoney(svName);
-		var svExecTime = ns.getHackTime(svName);
-		var svScore = (100 - svMinSec) * (svMaxMoney - svCurMoney) * svGrowth / svExecTime;
-		var svScore00 = ((100 - svMinSec) * svMaxMoney * svGrowth) / svExecTime;
-		var svScore01 = ((svMaxMoney - svCurMoney) / svGrowth) / svExecTime;
-		ns.print(""
-			+ "\r\n-------------------------------------"
-			+ "\r\nScore: " + svScore
-			+ "\r\nScore00: " + svScore00
-			+ "\r\nScore01: " + svScore01
-			+ "\r\nHost: " + svName
-			+ "\r\nMax Ram: " + svRAM + "GB"
-			+ "\r\nPorts: " + svPorts
-			+ "\r\nHack Time: " + svExecTime
-			+ "\r\nRequired Hacking: " + svReqHack
-			+ "\r\nMin Security: " + svMinSec
-			+ "\r\nServer Growth: " + svGrowth
-			+ "\r\nServer Money: " + ns.nFormat(svCurMoney, '$0,0.00')
-			+ "\r\nServer Max Money: " + ns.nFormat(svMaxMoney, '$0,0.00')
-			+ "\r\n" + rows[bestTargetIndex]
-			+ "\r\n-------------------------------------");
-	}
-
-	// Do things
-	lookForTargets(ns);
+	await processNmap(ns);
 }
