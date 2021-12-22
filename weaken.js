cf5773afbe6744d29df6aeb5656ab598feb1b168
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-21 22:44:20 -0600

    Update weaken.js

--- weaken.js
+++ weaken.js
@@ -1,7 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   const target = ns.args[0];
-  ns.tprint("Weakening " + target + " from " + ns.getHostname());
+  const useDebug = false;
+  if (useDebug) ns.tprint("Weakening " + target + " from " + ns.getHostname());
   while (true) {
     await ns.weaken(target);
   }
