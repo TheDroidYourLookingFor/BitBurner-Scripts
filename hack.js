17bb797a390129703dbf8dea2604474f65ada340
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-21 22:43:56 -0600

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,7 +1,8 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   const target = ns.args[0];
-  ns.tprint("Hacking " + target + " from " + ns.getHostname());
+  const useDebug = false;
+  if (useDebug) ns.tprint("Hacking " + target + " from " + ns.getHostname());
   while (true) {
     await ns.hack(target);
   }
