4ddfa55868ae2b877b1417a028ac498f5512e4e7
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-22 04:35:22 -0600

    Update Find.js

--- Find.js
+++ Find.js
@@ -1,91 +1,113 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	const args = ns.flags([['help', false]]);
-	const useDebug = true;
-	if (useDebug) ns.tail(ns.getScriptName());
 	const usrDirectory = "/TheDroid/";
-	const usrProbeData = new String("networkProbeData.txt");
-	const usrProbeData2 = new String("broke_Targets.txt");
-	const usrProbeData3 = usrDirectory + "best_target.txt";
-	var hackScripts = [usrDirectory + "weaken.js", usrDirectory + "hack.js", usrDirectory + "grow.js", usrDirectory + "aio.js"];
-	var hack_mem = ns.getScriptRam(usrDirectory + "weaken.js", "home");
-	var aio_mem = ns.getScriptRam(usrDirectory + "aio.js", "home");
+	const useDebug = false;
+	if (useDebug) ns.tail(ns.getScriptName());
 
-	if (args.help) {
-		ns.tprint("This script will distribute our script files to all hacked hosts.");
-		ns.tprint(`USAGE: run ${ns.getScriptName()}`);
-		ns.tprint("Example:");
-		ns.tprint(`> run ${ns.getScriptName()}`);
-		return;
+	const usrProbeData = usrDirectory + "best_target.txt";
+	const usrProbeData2 = usrDirectory + "networkProbeData.txt";
+	const usrProbeData3 = usrDirectory + "broke_Targets.txt";
+	
+	var myHackLevel = ns.getHackingLevel();
+	var numBusters = 0;
+	var portBusters = ['BruteSSH.exe', 'FTPCrack.exe', 'relaySMTP.exe', 'HTTPWorm.exe', 'SQLInject.exe'];
+	for (var i = 0; i < portBusters.length; i++) {
+		if (ns.fileExists(portBusters[i], "home")) ++numBusters;
 	}
 
 	/** @param {NS} ns **/
-	async function uploadToHost(svHost) {
-		if (useDebug) ns.print("Killing all processes on " + svHost);
-		ns.killall(svHost);
-		await ns.asleep(25);
-		if (useDebug) ns.print("Copying hackscripts to " + svHost);
-		await ns.scp(hackScripts, "home", svHost);
-		await ns.asleep(25);
+	function hackTarg(ns, svName, svPortsNeeded, svReqHack) {
+		if (useDebug) ns.tprint("Attempting to hack target: " + svName);
+		if (!(ns.hasRootAccess(svName)) && (numBusters >= svPortsNeeded) && (myHackLevel >= svReqHack)) {
+			if (useDebug) ns.tprint("Attempting to BruteSSH target: " + svName);
+			if (numBusters > 0) ns.brutessh(svName);
+			if (useDebug) ns.tprint("Attempting to FTPCrack target: " + svName);
+			if (numBusters > 1) ns.ftpcrack(svName);
+			if (useDebug) ns.tprint("Attempting to RelaySMTP target: " + svName);
+			if (numBusters > 2) ns.relaysmtp(svName);
+			if (useDebug) ns.tprint("Attempting to HTTPWorm target: " + svName);
+			if (numBusters > 3) ns.httpworm(svName);
+			if (useDebug) ns.tprint("Attempting to SQLInject target: " + svName);
+			if (numBusters > 4) ns.sqlinject(svName);
+			ns.nuke(svName);
+			//ns.installBackdoor();
+			if (useDebug) ns.tprint("Server hacked: " + svName);
+		}
 	}
 	/** @param {NS} ns **/
-	async function beginNetworkAttack(ns, fileName) {
-		if (useDebug) ns.print("Hack Memory: " + hack_mem);
-		if (useDebug) ns.print("AIO Memory: " + aio_mem);
+	async function lookForTargets(ns, fileName) {
+		if (useDebug) ns.tprint("Reading: " + fileName);
+		var bestTargetIndex = 15;
+		var bestTargetScore = 0;
 
-		var weakenThreadWeight = 15;
-		var hackThreadWeight = 55;
-		var growThreadWeight = 30;
-
-		var bestTarget = await ns.read(usrProbeData3).split(",");
-		var tName = bestTarget[0];
-		if (useDebug) ns.print("Best Target: " + tName);
-
-		var rows = await ns.read(usrDirectory + fileName).split("\r\n");
+		var rows = await ns.read(fileName).split("\r\n");
 		for (var i = 0; i < rows.length; ++i) {
 			var serverData = rows[i].split(',');
 			if (serverData.length < 9) break;
 			var svName = serverData[0];
-			var svRamAvail = ns.getServerMaxRam(svName);
-			var num_threads = Math.floor(svRamAvail / hack_mem);
+			var svRam = serverData[1];
+			var svPortsNeeded = serverData[2];
+			var svMinSec = serverData[3];
+			var svReqHack = serverData[4];
+			var svExecTime = serverData[5];
+			var svCurMoney = serverData[6];
+			var svMaxMoney = serverData[7];
+			var svGrowth = serverData[8];
+			var svScore;
 
-			if (ns.hasRootAccess(svName) && svName != "home") {
-				if (num_threads >= 6) {
-					if (useDebug) ns.print("Beginning multithreaded attack on" + tName + " from " + svName);
-					if ((num_threads & 1) != 0) num_threads = num_threads - hack_mem;
-					var hack_threads = Math.floor(((hackThreadWeight / 100) * num_threads));
-					var grow_threads = Math.floor(((growThreadWeight / 100) * num_threads));
-					var weaken_threads = Math.floor(((weakenThreadWeight / 100) * num_threads));
-					await uploadToHost(svName);
-					if (useDebug) ns.print("Executing " + hackScripts[1] + " with " + hack_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[1], svName, hack_threads, tName);
-					if (useDebug) ns.print("Executing " + hackScripts[0] + " with " + weaken_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[0], svName, weaken_threads, tName);
-					if (useDebug) ns.print("Executing " + hackScripts[2] + " with " + grow_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[2], svName, grow_threads, tName);
+			if (!ns.hasRootAccess(svName)) hackTarg(ns, svName, svPortsNeeded, svReqHack);
+
+			if (ns.hasRootAccess(svName) && (myHackLevel >= svReqHack)) {
+				if (svCurMoney < 50000 || svMaxMoney == 0) {
+					// They have no money to hack
 				} else {
-					if (svRamAvail > aio_mem) {
-						num_threads = Math.floor(svRamAvail / aio_mem);
-						if (useDebug) ns.print("Beginning low memory attack script on " + tName + " from " + svName);
-						if (num_threads >= 1) {
-							await uploadToHost(svName);
-							if (useDebug) ns.print("Executing " + hackScripts[3] + " with " + num_threads + " threads on " + tName + " from " + svName);
-							ns.exec(hackScripts[3], svName, num_threads, tName);
-						}
+					svScore = ((svMaxMoney * 100 / svGrowth) / svExecTime);
+					if (svScore > bestTargetScore) {
+						if (useDebug) ns.tprint("New High Score: " + bestTargetScore);
+						bestTargetScore = svScore;
+						bestTargetIndex = i;
 					}
 				}
 			}
-			ns.asleep(25);
 		}
+		await ns.write(usrProbeData, rows[bestTargetIndex], "w");
+		if (useDebug) outputStats(ns, svName);
+		ns.asleep(250);
 	}
-	/** @param {NS} ns **/
-	async function processNmap(ns) {
-		if (useDebug) ns.print("Beginning distribution of scripts to all servers.");
-		if (useDebug) ns.print("Reading " + usrProbeData);
-		await beginNetworkAttack(ns, usrProbeData);
-		if (useDebug) ns.print("Reading " + usrProbeData2);
-		await beginNetworkAttack(ns, usrProbeData2);
-		if (useDebug) ns.print("Finished distributing scripts to all servers.");
+
+	async function outputStats(ns, svrName) {
+		var svName = svrName;
+		var svRAM = ns.getServerMaxRam(svName);
+		var svPorts = ns.getServerNumPortsRequired(svName);
+		var svReqHack = ns.getServerRequiredHackingLevel(svName);
+		var svMinSec = ns.getServerMinSecurityLevel(svName);
+		var svGrowth = ns.getServerGrowth(svName);
+		var svCurMoney = ns.getServerMoneyAvailable(svName);
+		var svMaxMoney = ns.getServerMaxMoney(svName);
+		var svExecTime = ns.getHackTime(svName);
+		var svScore = (100 - svMinSec) * (svMaxMoney - svCurMoney) * svGrowth / svExecTime;
+		var svScore00 = ((100 - svMinSec) * svMaxMoney * svGrowth) / svExecTime;
+		var svScore01 = ((svMaxMoney - svCurMoney) / svGrowth) / svExecTime;
+		ns.tprint(""
+			+ "\r\n-------------------------------------"
+			+ "\r\nScore: " + svScore
+			+ "\r\nScore00: " + svScore00
+			+ "\r\nScore01: " + svScore01
+			+ "\r\nHost: " + svName
+			+ "\r\nMax Ram: " + svRAM + "GB"
+			+ "\r\nPorts: " + svPorts
+			+ "\r\nHack Time: " + svExecTime
+			+ "\r\nRequired Hacking: " + svReqHack
+			+ "\r\nMin Security: " + svMinSec
+			+ "\r\nServer Growth: " + svGrowth
+			+ "\r\nServer Money: " + ns.nFormat(svCurMoney, '$0,0.00')
+			+ "\r\nServer Max Money: " + ns.nFormat(svMaxMoney, '$0,0.00')
+			+ "\r\n" + rows[bestTargetIndex]
+			+ "\r\n-------------------------------------");
 	}
-	await processNmap(ns);
+
+	// Do things
+	lookForTargets(ns, usrProbeData3);
+	await ns.asleep(50);
+	lookForTargets(ns, usrProbeData2);
 }
