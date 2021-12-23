871f3826b8aba9332d95ac7b005780f97546b93a
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-23 16:00:57 -0600

    Update Find.js

--- Find.js
+++ Find.js
@@ -1,6 +1,7 @@
 /** @param {NS} ns **/
 import {
 	userDirectory,
+	usrProbeData00,
 	lookForBestTarget,
 	lookForHackableTargs
 } from "/TheDroid/TheDroid-Core.js";
@@ -8,13 +9,9 @@ import {
 /** @param {NS} ns **/
 export async function main(ns) {
 	let usrDir = userDirectory;
-	const usrProbeData2 = usrDir + "networkProbeData.txt";
-	const usrProbeData3 = usrDir + "broke_Targets.txt";
-	// Do things
-	await lookForHackableTargs(ns, usrProbeData2);
+	const usrProbeData = usrDir + usrProbeData00;
+	await lookForHackableTargs(ns, usrProbeData);
 	await ns.sleep(50);
-	await lookForBestTarget(ns, usrProbeData3);
-	await ns.sleep(50);
-	await lookForBestTarget(ns, usrProbeData2);
+	await lookForBestTarget(ns, usrProbeData);
 	await ns.sleep(50);
 }
