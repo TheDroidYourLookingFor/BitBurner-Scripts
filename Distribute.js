a303a6e8cbf38610a78ca9be2c5d5effc44aceff
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-23 12:25:44 -0600

    Update Distribute.js

--- Distribute.js
+++ Distribute.js
@@ -1,4 +1,14 @@
 /** @param {NS} ns **/
+import {
+	userDebug,
+	userDirectory,
+	beginNetworkAttack,
+	usrProbeData00,
+	usrProbeData01,
+	usrProbeData02
+} from "/TheDroid/TheDroid-Core.js";
+
+/** @param {NS} ns **/
 export async function main(ns) {
 	const args = ns.flags([['help', false]]);
 	if (args.help) {
@@ -8,97 +18,22 @@ export async function main(ns) {
 		ns.tprint(`> run ${ns.getScriptName()}`);
 		return;
 	}
-	const useDebug = false;
+	const useDebug = userDebug;
 	if (useDebug) ns.tail(ns.getScriptName())
-	const usrDirectory = "/TheDroid/";
-	const usrProbeData = new String("networkProbeData.txt");
-	const usrProbeData2 = new String("broke_Targets.txt");
-	var hackScripts = [usrDirectory + "weaken.js", usrDirectory + "hack.js", usrDirectory + "grow.js", usrDirectory + "aio.js"];
-	var hack_mem = ns.getScriptRam(usrDirectory + "weaken.js", "home");
-	var aio_mem = ns.getScriptRam(usrDirectory + "aio.js", "home");
-	const bestTarget = await ns.read(usrDirectory + "best_target.txt").split(",");
+	const usrProbeData = usrProbeData00;
+	const usrProbeData2 = usrProbeData01;
+	const usrDirectory = userDirectory;
+	const bestTarget = await ns.read(usrDirectory + usrProbeData02).split(",");
 	const targName = bestTarget[0];
 
 	/** @param {NS} ns **/
-	async function uploadToHost(svHost) {
-		if (useDebug) ns.tprint("Killing all processes on " + svHost);
-		ns.killall(svHost);
-		await ns.asleep(25);
-		if (useDebug) ns.tprint("Copying hackscripts to " + svHost);
-		await ns.scp(hackScripts, "home", svHost);
-		await ns.asleep(25);
-	}
-	/** @param {NS} ns **/
-	async function beginNetworkAttack(ns, fileName, tName) {
-		if (useDebug) ns.tprint("Hack Memory: " + hack_mem);
-		if (useDebug) ns.tprint("AIO Memory: " + aio_mem);
-
-		var weakenThreadWeight = 15;
-		var hackThreadWeight = 55;
-		var growThreadWeight = 30;
-
-		var rows = await ns.read(usrDirectory + fileName).split("\r\n");
-		for (var i = 0; i < rows.length; ++i) {
-			var serverData = rows[i].split(',');
-			if (serverData.length < 9) break;
-			var svName = serverData[0];
-			var svRamAvail = ns.getServerMaxRam(svName);
-			var num_threads = Math.floor(svRamAvail / hack_mem);
-
-			if (ns.hasRootAccess(svName) && svName != "home") {
-				if (num_threads >= 6) {
-					if (useDebug) ns.tprint("Beginning multithreaded attack on " + tName + " from " + svName + ".");
-					if ((num_threads & 1) != 0) num_threads = num_threads - hack_mem;
-					var hack_threads = Math.floor(((hackThreadWeight / 100) * num_threads));
-					var grow_threads = Math.floor(((growThreadWeight / 100) * num_threads));
-					var weaken_threads = Math.floor(((weakenThreadWeight / 100) * num_threads));
-					if (useDebug) ns.tprint("\r\n" + "Hack Threads: " + hack_threads
-						+ "\r\nGrow Threads: " + grow_threads
-						+ "\r\nWeaken Threads: " + weaken_threads
-					);
-
-					await uploadToHost(svName);
-					if (useDebug) ns.tprint("Executing " + hackScripts[1] + " with " + hack_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[1], svName, hack_threads, tName);
-					if (useDebug) ns.tprint("Executing " + hackScripts[0] + " with " + weaken_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[0], svName, weaken_threads, tName);
-					if (useDebug) ns.tprint("Executing " + hackScripts[2] + " with " + grow_threads + " threads on " + tName + " from " + svName);
-					ns.exec(hackScripts[2], svName, grow_threads, tName);
-					await ns.asleep(100);
-
-					svRamAvail = ns.getServerMaxRam(svName);
-					var svRamUsed = ns.getServerUsedRam(svName);
-					var num_threads = (svRamAvail - svRamUsed) / hack_mem;
-					if (num_threads > 0) {
-						ns.kill(hackScripts[1], svName, tName);
-						if (useDebug) ns.tprint("Executing " + hackScripts[1] + " with " + num_threads + " threads on " + tName + " from " + svName);
-						ns.exec(hackScripts[1], svName, (hack_threads + num_threads), tName);
-					}
-				} else {
-					if (svRamAvail > aio_mem) {
-						num_threads = Math.floor(svRamAvail / aio_mem);
-						if (useDebug) ns.tprint("Beginning low memory attack script on " + tName + " from " + svName);
-						await uploadToHost(svName);
-						if (num_threads > 0) {
-							if (useDebug) ns.tprint("Executing " + hackScripts[3] + " with " + num_threads + " threads on " + tName + " from " + svName);
-							ns.exec(hackScripts[3], svName, num_threads, tName);
-						}
-					}
-				}
-
-			}
-			ns.asleep(25);
-		}
-	}
-	/** @param {NS} ns **/
 	async function processNmap(ns) {
-
 		if (useDebug) ns.tprint("Beginning distribution of scripts to all servers.");
 		if (useDebug) ns.tprint("Best Target: " + targName);
 		if (useDebug) ns.tprint("Reading " + usrProbeData2);
-		await beginNetworkAttack(ns, usrProbeData2, targName);
+		await beginNetworkAttack(ns, false, usrProbeData2, targName);
 		if (useDebug) ns.tprint("Reading " + usrProbeData);
-		await beginNetworkAttack(ns, usrProbeData, targName);
+		await beginNetworkAttack(ns, false, usrProbeData, targName);
 		if (useDebug) ns.tprint("Finished distributing scripts to all servers.");
 	}
 
