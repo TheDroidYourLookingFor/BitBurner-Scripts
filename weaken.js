090febbdf53171c03d3f1f1d0cf12a95adab9372
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-24 16:45:38 -0600

    Update weaken.js

--- weaken.js
+++ weaken.js
@@ -1,9 +1,24 @@
 /** @param {NS} ns **/
+import {
+  userDebug,
+  attackSrvWeaken
+} from "/TheDroid/TheDroid-Core.js";
+
+/** @param {NS} ns **/
 export async function main(ns) {
+  const args = ns.flags([['help', false]]);
+  if (args.help) {
+    ns.tprint("This script will weaken our target automatically.");
+    ns.tprint(`USAGE: run ${ns.getScriptName()} TARGET_NAME`);
+    ns.tprint("Example:");
+    ns.tprint(`> run ${ns.getScriptName()} n00dles`);
+    return;
+  }
   const target = ns.args[0];
-  const useDebug = false;
-  if (useDebug) ns.tprint("Weakening " + target + " from " + ns.getHostname());
+  const useDebug = userDebug;
+  if (useDebug) ns.tprint("Weaken " + target + " from " + ns.getHostname());
   while (true) {
-    await ns.weaken(target);
+    await attackSrvWeaken(ns, target);
+    await ns.asleep(250);
   }
 }
