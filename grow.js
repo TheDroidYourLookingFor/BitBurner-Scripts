6800193686b1eeeb79b1a40aa0e803e687413033
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-24 16:44:18 -0600

    Update grow.js

--- grow.js
+++ grow.js
@@ -1,9 +1,24 @@
 /** @param {NS} ns **/
+import {
+  userDebug,
+  attackSrvGrow
+} from "/TheDroid/TheDroid-Core.js";
+
+/** @param {NS} ns **/
 export async function main(ns) {
+  const args = ns.flags([['help', false]]);
+  if (args.help) {
+    ns.tprint("This script will grow our target automatically.");
+    ns.tprint(`USAGE: run ${ns.getScriptName()} TARGET_NAME`);
+    ns.tprint("Example:");
+    ns.tprint(`> run ${ns.getScriptName()} n00dles`);
+    return;
+  }
   const target = ns.args[0];
-  const useDebug = false;
+  const useDebug = userDebug;
   if (useDebug) ns.tprint("Growing " + target + " from " + ns.getHostname());
   while (true) {
-    await ns.grow(target);
+    await attackSrvGrow(ns, target);
+    await ns.asleep(250);
   }
 }
