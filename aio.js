ccc8f2f0074cf5f24df2c82586f4e50fb31350e9
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2021-12-24 16:44:14 -0600

    Update aio.js

--- aio.js
+++ aio.js
@@ -1,17 +1,28 @@
 /** @param {NS} ns **/
+import {
+	userDebug,
+    srvGetMinSecurityLevel,
+    srvGetMaxMoney,
+    srvGetMoneyAvailable,
+    attackSrvHack,
+    attackSrvGrow,
+    attackSrvWeaken
+} from "/TheDroid/TheDroid-Core.js";
+
+/** @param {NS} ns **/
 export async function main(ns) {
     var target = ns.args[0];
-    const useDebug = false;
+    const useDebug = userDebug;
     if (useDebug) ns.tprint("Attacking " + target + " from " + ns.getHostname());
     while (true) {
-        var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-        var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
-            await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-            await ns.grow(target);
+        var moneyThresh = srvGetMaxMoney(ns, target) * 0.75;
+        var securityThresh = srvGetMinSecurityLevel(ns, target) + 5;
+        if (srvGetMinSecurityLevel(ns, target) > securityThresh) {
+            await attackSrvWeaken(ns, target);
+        } else if (srvGetMoneyAvailable(ns, target) < moneyThresh) {
+            await attackSrvGrow(ns, target);
         } else {
-            await ns.hack(target);
+            await attackSrvHack(ns, target);
         }
     }
 }
