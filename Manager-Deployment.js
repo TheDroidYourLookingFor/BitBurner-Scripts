0089c079e458bfe77129fd702493a724e51a8baa
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2022-01-02 17:33:52 -0600

    Update Manager-Deployment.js

--- Manager-Deployment.js
+++ Manager-Deployment.js
@@ -2,34 +2,36 @@
 import {
 	scriptCore,
 	scriptWHG,
+	deployTarget,
 	consoleMessage,
+	debugMessage,
 	lookForHackableTargs,
 	lookForBestTarget,
 	outputDeployment,
-	prepareHome,
+	networkAttack,
 	srvGetMaxMoney,
 	srvGetMinSecurityLevel,
 	srvGetSecurityLevel,
 	srvGetMoneyAvailable,
-	prepareTargets,
 	probeNetwork,
 	outputDeploymentCountdown,
-	deploymentCountdown,
-	setTotalThreads,
-	setTotalServers,
-	countTotalThreads
+	countTotalScriptThreads,
+	displayTotalThreads
 } from "/TheDroid/TheDroid-Core.js";
 
 var serverList = [];
+var attackList = ["foodnstuff", "joesguns", "sigma-cosmetics", "hong-fang-tea", "harakiri-sushi", "iron-gym"];
 /** @param {NS} ns **/
 export async function main(ns) {
 	const useAutoHack = true;
 	const useAutoFindBest = true;
 	const useHomeServer = true;
+	const deployMode00 = true;
+	const deployMode01 = false;
 	ns.tail();
 	ns.disableLog('ALL');
 	serverList = [];
-	var svTarget = "joesguns";
+	var svTarget = "n00dles";
 	var lastTarget = svTarget;
 	var lastMode = "weaken";
 
@@ -41,13 +43,14 @@ export async function main(ns) {
 		ns.tprint(`> run ${ns.getScriptName()}`);
 		return;
 	}
-	
-	outputDeploymentCountdown(ns, 0, true);
+
+	outputDeployment(ns, svTarget, lastMode);
+	// outputDeploymentCountdown(ns, 0, true);
 	while (true) {
 		if (useAutoHack) {
 			serverList = probeNetwork(ns);
 			lookForHackableTargs(ns, serverList);
-			await ns.asleep(5);
+			await ns.asleep(1);
 		}
 		if (useAutoFindBest) {
 			serverList = probeNetwork(ns);
@@ -55,46 +58,69 @@ export async function main(ns) {
 			if (lfbTarg != null) {
 				svTarget = lfbTarg;
 			}
-			await ns.asleep(5);
+			await ns.asleep(1);
 			if (svTarget != lastTarget) {
 				lastTarget = svTarget;
 				consoleMessage(ns, "New Target: " + lastTarget);
 			}
 		}
+		
+		outputDeployment(ns, svTarget, lastMode);
 
-		var moneyThresh = srvGetMaxMoney(ns, svTarget) * 0.75;
-		var securityThresh = srvGetMinSecurityLevel(ns, svTarget) + 5;
-
-		if (srvGetSecurityLevel(ns, svTarget) > securityThresh) {
-			if (lastMode != "weaken") {
-				lastMode = "weaken";
-				setTotalServers(0);
-				setTotalThreads(0);
-			}
-			if (useHomeServer) prepareHome(ns, scriptWHG[0], svTarget, 64, scriptWHG[1], scriptWHG[2]);
+		if (deployMode00) {
+			lastMode = "WHG"
 			serverList = probeNetwork(ns);
-			try { await prepareTargets(ns, serverList, scriptWHG[0], scriptCore, svTarget); } catch(e) { }
-		} else if (srvGetMoneyAvailable(ns, svTarget) < moneyThresh) {
-			if (lastMode != "grow") {
-				lastMode = "grow";
-				setTotalServers(0);
-				setTotalThreads(0);
-			}
-			if (useHomeServer) prepareHome(ns, scriptWHG[2], svTarget, 64, scriptWHG[0], scriptWHG[1]);
-			serverList = probeNetwork(ns);
-			try { await prepareTargets(ns, serverList, scriptWHG[2], scriptCore, svTarget); } catch(e) { }
-		} else {
-			if (lastMode != "attack") {
-				lastMode = "attack";
-				setTotalServers(0);
-				setTotalThreads(0);
-			}
-			if (useHomeServer) prepareHome(ns, scriptWHG[1], svTarget, 64, scriptWHG[0], scriptWHG[2]);
-			serverList = probeNetwork(ns);
-			try { await prepareTargets(ns, serverList, scriptWHG[1], scriptCore, svTarget); } catch(e) { }
+			debugMessage(ns, "Beginning distribution of scripts to all servers.");
+			debugMessage(ns, "Best Target: " + svTarget);
+			await networkAttack(ns, true, svTarget);
+			debugMessage(ns, "Finished distributing scripts to all servers.");
 		}
 
-		await ns.asleep(250);
+		if (deployMode01) {
+			serverList = probeNetwork(ns);
+			for (const svTarg of attackList) {
+				for (const svHost of serverList) {
+					var neededWeakenThreads;
+					var neededHackThreads;
+					var neededGrowThreads;
+					var wMem = ns.getScriptRam(scriptWHG[0]);
+					var hMem = ns.getScriptRam(scriptWHG[1]);
+					var gMem = ns.getScriptRam(scriptWHG[2]);
+					try { neededWeakenThreads = Math.ceil((srvGetSecurityLevel(ns, svTarg) - srvGetMinSecurityLevel(ns, svTarg)) * 20); } catch (e) { }
+					try { neededHackThreads = Math.ceil(ns.hackAnalyzeThreads(svTarg, srvGetMoneyAvailable(ns, svTarg))); } catch (e) { }
+					try { neededGrowThreads = Math.ceil(ns.growthAnalyze(svTarg, srvGetMaxMoney(ns, svTarg) / srvGetMoneyAvailable(ns, svTarg))); } catch (e) { }
+					var usedWeakenThreads = await countTotalScriptThreads(ns, svTarg, scriptWHG[0]);
+					var usedHackThreads = await countTotalScriptThreads(ns, svTarg, scriptWHG[1]);
+					var usedGrowThreads = await countTotalScriptThreads(ns, svTarg, scriptWHG[2]);
+
+					let availableThreads = (svHost.maxRam - svHost.ramUsed) / wMem;
+					if ((neededWeakenThreads - usedWeakenThreads) > usedWeakenThreads && availableThreads > 0) {
+						if (lastMode != "weaken") {
+							lastMode = "weaken";
+						}
+						await deployTarget(ns, svHost, scriptWHG[0], scriptCore, svTarg, neededWeakenThreads - usedWeakenThreads);
+					}
+
+					availableThreads = (svHost.maxRam - svHost.ramUsed) / hMem;
+					if ((neededHackThreads - usedHackThreads) > usedHackThreads && availableThreads > 0) {
+						if (lastMode != "hack") {
+							lastMode = "hack";
+						}
+						await deployTarget(ns, svHost, scriptWHG[1], scriptCore, svTarg, neededHackThreads - usedHackThreads);
+					}
+
+					availableThreads = (svHost.maxRam - svHost.ramUsed) / gMem;
+					if ((neededGrowThreads - usedGrowThreads) > usedGrowThreads && availableThreads > 0) {
+						if (lastMode != "grow") {
+							lastMode = "grow";
+						}
+						await deployTarget(ns, svHost, scriptWHG[2], scriptCore, svTarg, neededGrowThreads - usedGrowThreads);
+					}
+				};
+			}
+		}
+
 		outputDeployment(ns, svTarget, lastMode);
+		await ns.asleep(250);
 	}
 }
