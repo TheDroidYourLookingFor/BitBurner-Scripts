3c408412aae45b47420b8dad9a944c794c27590b
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2022-01-02 17:33:46 -0600

    Create Target-AIO.js

---
+++ Target-AIO.js
@@ -0,0 +1,29 @@
+/** @param {NS} ns **/
+import {
+	userDebug,
+    srvGetMinSecurityLevel,
+    srvGetSecurityLevel,
+    srvGetMaxMoney,
+    srvGetMoneyAvailable,
+    attackSrvHack,
+    attackSrvGrow,
+    attackSrvWeaken
+} from "/TheDroid/TheDroid-Core.js";
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    var target = ns.args[0];
+    const useDebug = userDebug;
+    if (useDebug) ns.tprint("Attacking " + target + " from " + ns.getHostname());
+    while (true) {
+        var moneyThresh = srvGetMaxMoney(ns, target) * 0.75;
+        var securityThresh = srvGetMinSecurityLevel(ns, target) + 5;
+        if (srvGetSecurityLevel(ns, target) > securityThresh) {
+            await attackSrvWeaken(ns, target);
+        } else if (srvGetMoneyAvailable(ns, target) < moneyThresh) {
+            await attackSrvGrow(ns, target);
+        } else {
+            await attackSrvHack(ns, target);
+        }
+    }
+}
