f2d3b29add0776129950b51b8e1cd9e0fcb5ee1c
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2022-01-03 08:16:46 -0600

    Update Manager-Startup.js

--- Manager-Startup.js
+++ Manager-Startup.js
@@ -9,9 +9,35 @@ export async function main(ns) {
 	var autoManageHackNet = true;
 	var autoManageStock = false;
 	var autoManageServers = true;
+	var autoBootOS = false;
 
 	ns.toast("Droid Scripts Startup beginning");
 
+	if (autoBootOS && ns.getHostname() == "home" && !ns.scriptRunning("/os/main.js", ns.getHostname())) {
+		ns.toast("Starting BootOS");
+		ns.run("/os/main.js", 1);
+	}
+
+	if (autoManageHacking && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Deployment.js", ns.getHostname())) {
+		ns.toast("Starting Manager-Deployment for auto hacking.")
+		ns.run(usrDirectory + "Manager-Deployment.js", 1);
+	}
+
+	if (autoManageServers && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Server.js", ns.getHostname())) {
+		ns.toast("Starting Manager-Server for Auto VPS Upgrading");
+		ns.run(usrDirectory + "Manager-Server.js", 1);
+	}
+
+	if (autoManageHackNet && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Hacknet.js", ns.getHostname())) {
+		ns.toast("Starting Manager-Hacknet for Auto Hacknet Upgrading");
+		ns.run(usrDirectory + "Manager-Hacknet.js", 1);
+	}
+
+	if (autoManageStock && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Stock.js", ns.getHostname())) {
+		ns.toast("Starting Manager-Stock for Auto Stock Trading.");
+		ns.run(usrDirectory + "Manager-Stock.js", 1);
+	}
+	
 	if (autoWindowMinimze && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Windows.js", ns.getHostname())) {
 		ns.toast("Starting Manager-Windows for window minimizing");
 		ns.run(usrDirectory + "Manager-Windows.js", 1);
@@ -30,25 +56,5 @@ export async function main(ns) {
 	if (autoSnow && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Snow.js", ns.getHostname())) {
 		ns.toast("Starting Manager-Snow to make it snow.");
 		ns.run(usrDirectory + "Manager-Snow.js", 1);
-	}
-
-	if (autoManageHacking && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Deployment.js", ns.getHostname())) {
-		ns.toast("Starting Manager-Deployment for auto hacking.")
-		ns.run(usrDirectory + "Manager-Deployment.js", 1);
-	}
-
-	if (autoManageHackNet && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Hacknet.js", ns.getHostname())) {
-		ns.toast("Starting Manager-Hacknet for Auto Hacknet Upgrading");
-		ns.run(usrDirectory + "Manager-Hacknet.js", 1);
-	}
-
-	if (autoManageServers && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Server.js", ns.getHostname())) {
-		ns.toast("Starting Manager-Server for Auto VPS Upgrading");
-		ns.run(usrDirectory + "Manager-Server.js", 1);
-	}
-
-	if (autoManageStock && ns.getHostname() == "home" && !ns.scriptRunning(usrDirectory + "Manager-Stock.js", ns.getHostname())) {
-		ns.toast("Starting Manager-Stock for Auto Stock Trading.");
-		ns.run(usrDirectory + "Manager-Stock.js", 1);
 	}
 }
