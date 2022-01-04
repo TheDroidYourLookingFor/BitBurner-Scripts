424d16cf5cbe1ea4d392f81fffc97a11a959157a
TheDroidYourLookingFor <90306870+TheDroidYourLookingFor@users.noreply.github.com>
2022-01-04 05:49:26 -0600

    Update Manager-Deployment.js

--- Manager-Deployment.js
+++ Manager-Deployment.js
@@ -1,31 +1,6 @@
 /** @param {NS} ns **/
 import {
 	scriptWHG,
-	consoleMessage,
-	debugMessage,
-	lookForHackableTargs,
-	lookForBestTarget,
-	outputDeployment,
-	networkAttack,
-	probeNetwork,
-	batch
-} from "/TheDroid/TheDroid-Core.js";
-
-var serverList = [];
-var attackerList = [];
-/** @param {NS} ns **/
-export async function main(ns) {
-	const batchLoops = 10;
-	const useAutoHack = true;
-	const useAutoFindBest = false;
-	const useHomeServer = true;
-	const deployMode00 = false;
-	const deployMode01 = true;
-	ns.tail();
-	ns.disableLog('ALL');
-	serverList = [];/** @param {NS} ns **/
-import {
-	scriptWHG,
 	scriptAll,
 	consoleMessage,
 	debugMessage,
@@ -50,8 +25,8 @@ export async function main(ns) {
 	ns.tail();
 	ns.disableLog('ALL');
 	serverList = [];
-	//var svTarget = "the-hub";
-	var svTarget = "n00dles";
+	var svTarget = "the-hub";
+	//var svTarget = "joesguns";
 	var lastTarget = svTarget;
 	var lastMode = "weaken";
 
@@ -101,7 +76,7 @@ export async function main(ns) {
 			for (const svHost of serverList) {
 				if (svHost.hasAdminRights) {
 					attackerList.push(svHost);
-					if (svHost.hostname != "home") await ns.scp(scriptAll,"home",svHost.hostname);
+					if (svHost.hostname != "home") await ns.scp(scriptAll, "home", svHost.hostname);
 					await ns.sleep(1);
 					svHostCount++;
 				}
